b57b4d85cce643c7d5ba98db59754306fb9f595d
Thor Lock Ryvard <Thry@itu.dk>
2021-02-23 08:54:47 +0100

    Accepted

--- Python/kattis/Autori/Autori.py
+++ Python/kattis/Autori/Autori.py
@@ -3,8 +3,10 @@
 import sys
 
 for i in sys.stdin:
+    #split by '-'
     name = i.split('-')
     newName = ''
     for x in name:
+        #take first char from split-array
         newName += x[0]
     print(newName)
